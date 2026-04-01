9e2da36614e549822a4ca818ed0cbacdceba41c1
FIZZA FATIMA <fizza.abbaspk@gmail.com>
2026-04-01 07:43:42 +0500

    ok backend

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const io = new Server(server, {
     cors: { origin: "*" } 
 });
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://vibenet-frontent.vercel.app", "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 // Server check karne ke liye root route
 app.get("/", (req, res) => {
